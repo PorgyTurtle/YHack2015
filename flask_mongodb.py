9a5fc9bbed55d6139c8ce88105629947deb431c6
taisuke-yasuda <tai.taisuke.yasuda@gmail.com>
2015-11-06 23:50:32 -0500

    Upload

---
+++ flask_mongodb.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask.ext.pymongo import PyMongo
+from flask import render_template
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+client = MongoClient()
+db = client.test
+
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+if __name__ == '__main__':
+    app.run()
